8d998d1f0c7ff91a0491c0f4d9730cbbbe31e071
adamkhairi <57996941+adamkhairi@users.noreply.github.com>
2021-06-09 23:02:50 +0100

    Add project files.

---
+++ Helpers/AppUserProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Store_Shared.Dto;
+using Store_Shared.Models;
+
+namespace Store_API.Helpers
+{
+    public class AppUserProfile : Profile
+    {
+        public AppUserProfile()
+        {
+            CreateMap<ApplicationUser, RegisterModel>();
+            CreateMap<RegisterModel, ApplicationUser>();
+            CreateMap<ApplicationUser, AuthModel>();
+            CreateMap<ApplicationUser, ApplicationUser>();
+            CreateMap<EditUserModel, RegisterModel>();
+            CreateMap<ApplicationUser, UserModel>();
+        }
+    }
+}
---
+++ Services/Auth/IAuthService.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Store_Shared.Dto;
+
+namespace Store_API.Services.Auth
+{
+    public interface IAuthService
+    {
+        Task<AuthModel> RegisterAsync(RegisterModel model);
+        Task<AuthModel> GetTokenAsync(TokenRequestModel model);
+        Task<string> AddRoleAsync(AddRoleModel model);
+        Task<List<object>> GetRolesList();
+        Task<List<object>> GetUsersList();
+    }
+
+    //public class AddRoleModel
+    //{
+    //}
+}
